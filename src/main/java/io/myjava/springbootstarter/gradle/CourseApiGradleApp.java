f2aa26e02db89ba6152719e78b3d3680c5f265e3
rpv <rpv@rpvs-MacBook-Pro.local>
2018-10-07 16:34:49 +0530

    Initial Commit

---
+++ src/main/java/io/myjava/springbootstarter/gradle/CourseApiGradleApp.java
@@ -0,0 +1,13 @@
+package io.myjava.springbootstarter.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiGradleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiGradleApp.class, args);
+	}
+
+}
